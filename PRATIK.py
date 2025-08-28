67165837540702fb86082f69e226739beb5ee420
PRATIK475 <officialpratik25@gmail.com>
2025-08-28 16:55:26 +0530

    Create PRATIK.py
    Added prime number code

---
+++ PRATIK.py
@@ -0,0 +1,14 @@
+#import math
+num = int(input("Enter an integer: "))
+if num <= 1:
+    print(f"{num} is not prime")
+else:
+    result = True
+    for i in range(2, int(math.sqrt(num)) + 1):
+     if num % i == 0:
+         result = False
+         break
+    if result :
+        print(f"{num} is prime")
+    else:
+        print(f"{num} is not prime")
